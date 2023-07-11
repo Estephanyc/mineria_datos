f3acea3b85124b49823f4d7ffd71e7adea80feac
Estephany Carvajal Hernandez <estephany@rindegastos.com>
2023-07-11 08:51:52 -0400

    Validacion de rut

--- createObject.py
+++ createObject.py
@@ -1,17 +1,17 @@
 def createObject(row):
 
     object = {
-    'n_boleta': row[0],
-    'fecha_venta':	row[1],
-    'codigo_medicamento': row[2],
-    'nombre_medicamento': row[3],
-    'clasificacion_de_origen': row[4],
-    'tipo_venta': row[5],
-    'unidades_vendidas': row[6],
-    'n_local': row[7],
-    'direccion': row[8],
-    'rut_cliente': row[9],
-    'clasificacion': row[10],
-    'nombre_cliente': row[11]
+    'n_boleta': row[0].strip(),
+    'fecha_venta':	row[1].strip(),
+    'codigo_medicamento': row[2].strip(),
+    'nombre_medicamento': row[3].strip(),
+    'clasificacion_de_origen': row[4].strip(),
+    'tipo_venta': row[5].strip(),
+    'unidades_vendidas': row[6].strip(),
+    'n_local': row[7].strip(),
+    'direccion': row[8].strip(),
+    'rut_cliente': row[9].strip(),
+    'clasificacion': row[10].strip(),
+    'nombre_cliente': row[11].strip()
    }
     return object
