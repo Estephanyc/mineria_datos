e90ad514df3684be3624f52d3ac229e34b6a3c1a
Francisca <fr.perez@live.cl>
2023-07-11 22:01:54 -0400

    tipo venta

--- normalizationFunctions/tipoVenta.py
+++ normalizationFunctions/tipoVenta.py
@@ -1,5 +1,28 @@
 def tipoVenta(object):
-    
-    # Acá debe quedar el tipo de venta normalizado
-    object['tipo_venta'] = '1'
+     # Eliminar espacios
+    tipoVenta = object['tipo_venta'].strip()
+        # Convertir a minuscula
+    tipoVenta = tipoVenta.lower()
+    # Reemplazar letras con tildes
+    replacements = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "i"),
+        ("ó", "o"),
+        ("ú", "u"),
+    )
+
+    for a, b in replacements:
+        tipoVenta = tipoVenta.replace(a, b)
+
+    if tipoVenta == 'receta obligatoria':
+        object['tipo_venta'] = '3'
+    elif tipoVenta == 'receta retenida':
+        object['tipo_venta'] = '1'
+    elif tipoVenta == 'libre':
+        object['tipo_venta'] = '2'
+    else:
+        pass
+     
+
     
